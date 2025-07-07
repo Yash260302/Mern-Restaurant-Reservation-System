c24aa62b33ed3aec38d21de87588d89ca4cbc7cf
Yash260302 <166744625+Yash260302@users.noreply.github.com>
2025-07-07 14:52:30 +0530

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,16 +10,17 @@ const About = () => {
           <div className="banner">
             <div className="top">
               <h1 className="heading">ABOUT US</h1>
-              <p>The only thing we're serious about is food.</p>
+              <p>No Jokes, When it comes to Food.</p>
             </div>
             <p className="mid">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident possimus optio adipisci dolores impedit illum iusto
-              perferendis, laudantium quod accusamus consequuntur consectetur,
-              tempore nulla error iure reiciendis dolorem assumenda.
-              Necessitatibus fugit asperiores totam rem esse exercitationem
-              iusto ipsum qui dolore ex, accusantium repellat mollitia
-              repellendus.
+              At Plateful, we believe every meal deserves a perfect seat. 
+              We connect diners to restaurants with a seamless reservation 
+              system that makes booking your next table simple, fast, and 
+              reliable. Whether you’re planning a cozy dinner, a lively 
+              group gathering, or a special occasion, Plateful makes it easy to 
+              discover and reserve the best spots in town. We’re passionate 
+              about food, dedicated to service, and serious about making 
+              every dining experience memorable, one plate at a time.
             </p>
             <Link to={"/"}>
               Explore Menu{" "}
