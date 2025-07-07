3590a2462b5a1eeddab28ebe9b8e6dfbdc08cb23
Yash260302 <166744625+Yash260302@users.noreply.github.com>
2025-07-07 14:46:19 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">PLATEFUL</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
-            <p>Open: 05:00 PM - 12:00 AM</p>
+            <p>M6, Hazratganj, Lucknow</p>
+            <p>Open: 11:00 AM - 10:00 PM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Yashasvi Verma</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Yashasvi Verma.</p>
           </div>
         </div>
       </div>
